745235220d6b24d62d617614b3cb193126bfa8d5
esraras <eesraras29@gmail.com>
2026-09-11 13:26:49 +0300

    BookingView and Slider added

--- FlightBooking/obj/Debug/net10.0/FlightBooking.AssemblyInfo.cs
+++ FlightBooking/obj/Debug/net10.0/FlightBooking.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0082832f0b80c749dc436b51ef64c32376855af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+283f9fee0f301a289b638efa138ab26df2feadff")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
